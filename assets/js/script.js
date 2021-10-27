5b5217338e75ce47043cac31eaba61b483a6bf3b
Dustin Grijalva <dustin2400@hotmail.com>
2021-10-27 18:27:26 -0500

    removed icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 
 
-// fetch("https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=132271845&apikey=d428aa06c4361e771f014b185e274928").then(function(response) {
-//     response.json().then(function(data) {
-//         console.log(data);
-//         var lyrics = data.message.body.lyrics.lyrics_body;
-//         console.log(lyrics);
-//     });
-// });
+fetch("https://api.genius/search?q=Kendrick%20Lamar").then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+        var lyrics = data.message.body.lyrics.lyrics_body;
+        console.log(lyrics);
+    });
+});
 
