0478806d0763e823442019af6481e684fbdc5b4b
Dustin Grijalva <dustin2400@hotmail.com>
2021-11-01 19:02:44 -0500

    added info functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,50 +64,5 @@ function displayLyricsLink(data) {
 	lyricsEl.innerHTML = data.response.song.embed_content;
 }
 
-// function displayFacts(data) {
-//     var facts = data.response.song.description.dom.children
-//     for (i=0; i<facts.length; i++) {
-//         console.log(facts[i].children);
-//     }
-    
-// }
-// fetch("https://api.genius.com/search?access_token=zJR5ej6XMOAz17iEUDh32hGpZCmgwt7yNN3radinKSnI3i-Sx60laizqFjIEQkaN&q=Kendrick%20Lamar%20king%20kunta")
-// .then(response => response.json()).then(json => {
-//     console.log(json);
-// });
-
-// fetch("https://api.genius.com/songs/3039923?access_token=zJR5ej6XMOAz17iEUDh32hGpZCmgwt7yNN3radinKSnI3i-Sx60laizqFjIEQkaN")
-// .then(function(response) {
-//     response.json().then(function(data) {
-//         console.log(data);
-//     });
-// });
-
-var lyrics = document.querySelector("#lyrics")
-// fetch("https://sridurgayadav-chart-lyrics-v1.p.rapidapi.com/apiv1.asmx/SearchLyricDirect?artist=michael%20jackson&song=thriller", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "sridurgayadav-chart-lyrics-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "74651c6c27msh2d27b3fd0293ce1p1513e2jsn17527c211e3f"
-// 	}
-// })
-// fetch("https://api.chartLyrics.com/apiv1.asmx/SearchLyricDirect?artist=michael%20jackson&song",{
-//     "method": "GET",
-//     "HTTP": "1.1"
-// })
-
-
-// .then(function(response) {
-//     response.text().then(function(data){
-//         lyrics.innerHTML = data;
-//     })
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
-// setTimeout(function() {
-//     var lyric = document.getElementsByTagName("lyric");
-//     console.log(lyric[0].innerHTML);
-// }, 1000);
 
 searchButton.addEventListener("click", searchHandler);
